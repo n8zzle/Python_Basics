0e6b50c34038aebe89ea94b6cd3c86aae216a640
Игорь Чичкаленко <iitz4i4a@Igors-MacBook-Pro.local>
2025-04-24 13:40:07 +0200

    first small lesson

---
+++ Python Tutorial/syntax.py
@@ -0,0 +1,39 @@
+# Variables
+x = 5
+message = "Hello, World !"
+
+print("Variable x:", x)
+print("Variable message:", message)
+
+# If you want to specify the data type of a variable
+y = str(3)  # y will be '3'
+print("Variable y:", y)
+z = float(3)  # z will be 3.0
+print("Variable z:", z)
+v = int(3)  # v will be 3
+print("Variable v:", v)
+
+# As well we can get the data type of a variable with type()
+igors = "Igors"  # String
+print("Type of igors: ", type(igors))
+age = 26  # Number INT
+print("Type of age: ", type(age))
+city = "Berlin"  # Sting
+print("Type of city", type(city))
+badHabbits = False  # Boolean
+print("Type of badHabbits", type(badHabbits))
+cat = True  # Boolean
+print("Type of cat", type(cat))
+
+# IF and ELSE - statement
+if 5 > 2:
+    print("Five is greater than two")
+else:
+    print("Two is greater that five")
+
+# Case-Sensitive - this will create two variables
+number = 4
+name = "Sally"
+
+print(number)
+print(name)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Python_Basics
+
+Link: <https://www.w3schools.com/python/python_syntax.asp>
+
+Next Lesson: Variable Names
