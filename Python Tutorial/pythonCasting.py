39136be1dfa6c5e844b174cfa5af5a80183cf76f
Игорь Чичкаленко <iitz4i4a@Igors-MacBook-Pro.local>
2025-04-25 16:21:12 +0200

    Lesson - Casting

---
+++ Python Tutorial/pythonCasting.py
@@ -0,0 +1,19 @@
+# There may be times when you want to specify a type on to a variable.
+# This can be done with casting.
+# Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+
+# Int Example
+x = int(1)
+x = int(2.8)
+x = int("3")
+
+# Float Example
+x = float(1)
+x = float(2.8)
+x = float("3")
+x = float("4.2")
+
+# String Example
+x = str("s1")
+x = str(2)
+x = str(3.0)
