6f626851df1ede6e5e2765e8b4eea843949c3291
Игорь Чичкаленко <iitz4i4a@Igors-MacBook-Pro.local>
2025-04-25 10:32:56 +0200

    Lesson - Python Data Types

---
+++ Python Tutorial/DataTyes.py
@@ -0,0 +1,31 @@
+# Data Types
+x = "Hello World"
+print(type(x))
+x = 20
+print(type(x))
+x = 20.5
+print(type(x))
+x = 1j
+print(type(x))
+x = ["apple", "banana", "cherry"]
+print(type(x))
+x = ("MacOS", "Linux", "Windows")
+print(type(x))
+x = range(6)
+print(type(x))
+x = {"name": "Igors", "age": 26}
+print(type(x))
+x = {"apple", "banana", "cherry"}
+print(type(x))
+x = frozenset({"user", "admin", "superadmin"})
+print(type(x))
+x = True
+print(type(x))
+x = b"Hello"
+print(type(x))
+x = bytearray(5)
+print(type(x))
+x = memoryview(bytes(5))
+print(type(x))
+x = None
+print(type(x))
