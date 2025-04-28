20ed4195708fd151e54c939e6e26cfc8a1804da8
n8zzle <playwithn8zzle@gmail.com>
2025-04-28 12:51:37 +0200

    Starting to learn about python tuples
    They are kind a simmilar with arrays

---
+++ Python Tutorial/Python Tuple/pythonAccessTuple.py
@@ -0,0 +1,11 @@
+# You can access tuple items by referring to the index number, inside square brackets:
+mytuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(mytuple[0])
+print(mytuple[-1])
+print(mytuple[2:5])
+print(mytuple[:3])
+print(mytuple[3:])
+print(mytuple[-4:-1])
+
+if "melon" in mytuple:
+    print("Yes,'melon' is  in the mytuple ")
---
+++ Python Tutorial/Python Tuple/pythonTuples.py
@@ -0,0 +1,17 @@
+mytuple = ("apple", "cherry", "banana")
+print(mytuple)
+print(mytuple[0])
+print(mytuple[1])
+print(mytuple[2])
+print(len(mytuple))
+
+# Tuple items can be of any data type
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+tuple4 = ("abc", 34, True, 40, "male")
+print(type(tuple1))
+
+# We can create tuple with tuple() method:
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
