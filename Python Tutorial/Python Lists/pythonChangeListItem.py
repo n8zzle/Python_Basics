622b68526ab8399e45fc97a224da27fd09bfd236
n8zzle <playwithn8zzle@gmail.com>
2025-04-28 11:04:01 +0200

    Lesson - Python Change List Item

---
+++ Python Tutorial/Python Lists/pythonChangeListItem.py
@@ -0,0 +1,13 @@
+# Changing list item
+mylist = ["apple", "orange", "kiwi"]
+mylist[1] = "cherry"
+print(mylist)
+
+# Change a Range of Item Values and insert new
+mylist[0:3] = ["Igors", "Cickalenko", "Berlin"]
+print(mylist)
+
+# Insert "watermelon" as the third item:
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
